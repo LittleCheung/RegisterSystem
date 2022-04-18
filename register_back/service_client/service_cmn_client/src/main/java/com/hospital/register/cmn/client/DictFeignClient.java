f865153b9f9698ea64721c52fb6823e4d4d46372
LittleCheung <dongcarry@126.com>
2022-04-18 23:06:12 +0800

    修改对数据字典模块的远程服务调用接口

--- register_back/service_client/service_cmn_client/src/main/java/com/hospital/register/cmn/client/DictFeignClient.java
+++ register_back/service_client/service_cmn_client/src/main/java/com/hospital/register/cmn/client/DictFeignClient.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface DictFeignClient {
 
     /**
-     * 根据dictcode和value查询
+     * 根据dictcode和value获取数据字典名称
      * @param dictCode
      * @param value
      * @return
@@ -24,7 +24,7 @@ public interface DictFeignClient {
     public String getName(@PathVariable("dictCode") String dictCode, @PathVariable("value") String value);
 
     /**
-     * 根据value查询
+     * 根据value获取数据字典名称
      * @param value
      * @return
      */
